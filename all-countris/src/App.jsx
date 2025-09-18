c30889bc5096857ab0a66df7d273a3facee7abc0
unknown <you@example.com>
2025-09-18 22:32:09 +0600

    first commit

---
+++ all-countris/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countris from './Components/Countrys/Countris'
+
+function App() {
+  
+
+  return (
+    <>
+      <Countris></Countris>
+    </>
+  )
+}
+
+export default App
